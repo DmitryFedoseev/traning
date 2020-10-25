6861e05781fff1d294d463d58a85da67d0e3c534
Dmitry <feddiman02@gmail.com>
2020-10-25 23:07:55 +0300

    Added function sum

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+int sum(a, b);
+
 int main(){
     printf("Hello, World!\n");
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
     return 0;
 }
+int sum(a, b){
+     int y;
+     y = a + b;
+     return(y);
+ }
