0dc053ccedff1233a1dd41e3839a4ceecb153c82
Dmitry <feddiman02@gmail.com>
2020-10-25 23:07:55 +0300

    Added math function

--- main.c
+++ main.c
@@ -1,6 +1,61 @@
 #include <stdio.h>
 
+int sum(a, b);
+int raz(a, b);
+int um(a, b);
+float del(a, b);
+int test(a, b);
+
 int main(){
     printf("Hello, World!\n");
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    test(a, b);
     return 0;
+}
+int sum(a, b){
+     int y;
+     y = a + b;
+     return(y);
+ }
+int raz(a, b){
+     int y;
+     y = a - b;
+     return(y);
+ }
+ int um(a, b){
+     int y;
+     y = a * b;
+     return(y);
+ }
+ float del(a, b){
+     float y;
+     y = (float) a / b;
+     return(y);
+ }
+int test(a, b)
+{
+        if (a + b == sum(a, b))
+        {
+          printf("%d + %d = %d the answer is correct", a, b, sum(a, b));
+          printf("\n");
+        }
+        if (a - b == raz(a, b))
+        {
+          printf("%d - %d = %d the answer is correct", a, b, raz(a, b));
+          printf("\n");
+        }
+        float z;
+         z = (float) a / b;
+        if (z == del(a, b))
+        {
+          printf("%d / %d = %f the answer is correct", a, b, del(a, b));
+          printf("\n");
+        }
+        if (a * b == um(a, b))
+        {
+          printf("%d * %d = %d the answer is correct", a, b, um(a, b));
+          printf("\n");
+        }
 }
